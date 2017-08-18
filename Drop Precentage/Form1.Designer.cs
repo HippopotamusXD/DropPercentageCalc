1ec47a74aa0dc927af46acc302eccb076471ca01
Simun <numis.bat@gmial.com>
2017-08-18 21:53:00 +0200

    Bug when inputing decimal numbers

--- Drop Precentage/Form1.Designer.cs
+++ Drop Precentage/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Drop_Precentage
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tlpMain = new System.Windows.Forms.TableLayoutPanel();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblAveragePercentage = new System.Windows.Forms.Label();
+            this.lblYourPercentage = new System.Windows.Forms.Label();
+            this.cbDropped = new System.Windows.Forms.CheckBox();
+            this.tlpMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tlpMain
+            // 
+            this.tlpMain.ColumnCount = 4;
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlpMain.Controls.Add(this.tbInput, 0, 0);
+            this.tlpMain.Controls.Add(this.btnAdd, 0, 1);
+            this.tlpMain.Controls.Add(this.btnClear, 3, 1);
+            this.tlpMain.Controls.Add(this.lblAveragePercentage, 2, 0);
+            this.tlpMain.Controls.Add(this.lblYourPercentage, 3, 0);
+            this.tlpMain.Controls.Add(this.cbDropped, 1, 1);
+            this.tlpMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpMain.Location = new System.Drawing.Point(0, 0);
+            this.tlpMain.Name = "tlpMain";
+            this.tlpMain.RowCount = 2;
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpMain.Size = new System.Drawing.Size(359, 87);
+            this.tlpMain.TabIndex = 0;
+            // 
+            // tbInput
+            // 
+            this.tbInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tlpMain.SetColumnSpan(this.tbInput, 2);
+            this.tbInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbInput.Location = new System.Drawing.Point(3, 3);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(172, 20);
+            this.tbInput.TabIndex = 0;
+            this.tbInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnAdd.Location = new System.Drawing.Point(3, 46);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(83, 38);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnClear.Location = new System.Drawing.Point(270, 46);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(86, 38);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lblAveragePercentage
+            // 
+            this.lblAveragePercentage.AutoSize = true;
+            this.lblAveragePercentage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblAveragePercentage.Location = new System.Drawing.Point(181, 0);
+            this.lblAveragePercentage.Name = "lblAveragePercentage";
+            this.lblAveragePercentage.Size = new System.Drawing.Size(83, 43);
+            this.lblAveragePercentage.TabIndex = 3;
+            this.lblAveragePercentage.Text = "Average";
+            this.lblAveragePercentage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblYourPercentage
+            // 
+            this.lblYourPercentage.AutoSize = true;
+            this.lblYourPercentage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblYourPercentage.Location = new System.Drawing.Point(270, 0);
+            this.lblYourPercentage.Name = "lblYourPercentage";
+            this.lblYourPercentage.Size = new System.Drawing.Size(86, 43);
+            this.lblYourPercentage.TabIndex = 4;
+            this.lblYourPercentage.Text = "Your";
+            this.lblYourPercentage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cbDropped
+            // 
+            this.cbDropped.AutoSize = true;
+            this.tlpMain.SetColumnSpan(this.cbDropped, 2);
+            this.cbDropped.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cbDropped.Location = new System.Drawing.Point(92, 46);
+            this.cbDropped.Name = "cbDropped";
+            this.cbDropped.Size = new System.Drawing.Size(172, 38);
+            this.cbDropped.TabIndex = 5;
+            this.cbDropped.Text = "Dropped";
+            this.cbDropped.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(359, 87);
+            this.Controls.Add(this.tlpMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Drop Percentage";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.tlpMain.ResumeLayout(false);
+            this.tlpMain.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlpMain;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblAveragePercentage;
+        private System.Windows.Forms.Label lblYourPercentage;
+        private System.Windows.Forms.CheckBox cbDropped;
     }
 }
 
--- Drop Precentage/Form1.cs
+++ Drop Precentage/Form1.cs
@@ -1,20 +1,191 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+using System.Windows.Forms;
+using System.Data.OleDb;
 using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
 namespace Drop_Precentage
 {
-    public partial class Form1 : Form
+    public partial class FormMain : Form
     {
-        public Form1()
+        OleDbConnection MainConnection = new OleDbConnection();
+
+        public FormMain()
         {
             InitializeComponent();
         }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            ConnectLocalDatabase("DropPercentage.mdb");
+        }
+
+        #region Connecting to the database
+        private bool ConnectLocalDatabase(string database)
+        {
+            string connetionString = "";
+            try
+            {
+                connetionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + database + ";" + "Mode=ReadWrite;";
+                MainConnection.ConnectionString = connetionString;
+                if (!OpenDatabase())
+                {
+                    connetionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + database + ";" + "Mode=ReadWrite;";
+                    MainConnection.ConnectionString = connetionString;
+                    if (!OpenDatabase())
+                    {
+                        return false;
+                    }
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString());
+                return false;
+            }
+            return true;
+        }
+
+        private bool OpenDatabase()
+        {
+            try
+            {
+                MainConnection.Open();
+                return true;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString());
+                return false;
+            }
+        }
+        #endregion
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            float perc;
+            if (float.TryParse(tbInput.Text, out perc) && perc <= 100 && perc >= 2)
+            {
+                try
+                {
+                    string sql = "";
+                    sql = "INSERT INTO Percentages (Dropped, Percentage) VALUES (" + cbDropped.Checked + ", " + perc + ")";
+
+                    OleDbCommand cmd = new OleDbCommand(sql, MainConnection);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Added");
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show(err.ToString());
+                }
+                calcAverage();
+                calcYour();
+                updateColor();
+                tbInput.Text = "";
+                cbDropped.Checked = false;
+            }
+            else
+            {
+                MessageBox.Show("Invalid input");
+            }
+        }
+
+        #region Calculating percentages
+        private void calcAverage()
+        {
+            try
+            {
+                string sql = "SELECT ROUND(AVG(Percentage), 2) FROM Percentages";
+                using (IDbCommand command = new OleDbCommand(sql, MainConnection))
+                {
+                    object result = command.ExecuteScalar();
+                    lblAveragePercentage.Text = Convert.ToString(result);
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString());
+            }
+        }
+
+        private void calcYour()
+        {
+            float total;
+            float dropped;
+            float percentage;
+            try
+            {
+                string sql = "SELECT COUNT(Percentage) FROM Percentages";
+                using (IDbCommand command = new OleDbCommand(sql, MainConnection))
+                {
+                    object result = command.ExecuteScalar();
+                    total = (int)(result);
+                }
+                sql = "SELECT Count(Percentage) FROM Percentages WHERE dropped = true";
+                using (IDbCommand command = new OleDbCommand(sql, MainConnection))
+                {
+                    object result = command.ExecuteScalar();
+                    dropped = (int)(result);
+                }
+                percentage = dropped / total * 100;
+
+                if (!float.IsNaN(percentage))
+                {
+                    lblYourPercentage.Text = percentage.ToString("0.00");
+                }
+                else
+                {
+                    lblYourPercentage.Text = "No record";
+                }
+
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString());
+            }
+        }
+        #endregion
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Are you sure you want to delete everything?", "Are you sure you want to delete everything?", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                UseWaitCursor = true;
+                string sql = "";
+                sql = "DELETE FROM Percentages";
+
+                OleDbCommand cmd = new OleDbCommand(sql, MainConnection);
+                cmd.ExecuteNonQuery();
+                UseWaitCursor = false;
+
+                lblAveragePercentage.Text = "0.00";
+                lblYourPercentage.Text = "0.00";
+                lblYourPercentage.BackColor = Color.White;
+            }
+        }
+
+        private void updateColor()
+        {
+            float yourPerc;
+            float avgPerc;
+
+            yourPerc = float.Parse(lblYourPercentage.Text);
+            avgPerc = float.Parse(lblAveragePercentage.Text);
+
+            if (yourPerc > 1.1 * avgPerc)
+            {
+                lblYourPercentage.BackColor = Color.Green;
+            }
+            else if (avgPerc > 1.1 * yourPerc)
+            {
+                lblYourPercentage.BackColor = Color.Red;
+            }
+            else
+            {
+                lblYourPercentage.BackColor = Color.Yellow;
+            }
+        }
+
     }
 }
